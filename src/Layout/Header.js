f44cb280f76818d462694afa6444faf3ca8f0d0d
Stanley Cooper <scooperlu@gmail.com>
2022-03-16 20:43:13 -0400

    basic header created

---
+++ src/Layout/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div class="flex justify-between items-center bg-sky-900 px-10">
+        <h1 class="text-slate-100">Estimator Helper</h1> 
+        <img class="scale-50" src="/companyLogo.png" alt="company logo" />
+    </div>
+  )
+}
